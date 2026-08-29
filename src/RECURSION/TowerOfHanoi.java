d836cad03924b557b27f60d298d3117f2f295434
Git-soumik1802 <128280509+Git-soumik1802@users.noreply.github.com>
2026-08-29 19:36:12 +0530

    Sync packages with Eclipse project

--- src/RECURSION/TowerOfHanoi.java
+++ src/RECURSION/TowerOfHanoi.java
@@ -1,12 +1,29 @@
 package RECURSION;
 
 public class TowerOfHanoi {
-	public static void TOW() {
-		
-	}
-	public static void main(String[] args) {
-		
 
-	}
+    public static void TOW(int n, int src, int helper, int dest) {
 
+        // Base case
+        if (n == 1) {
+            System.out.println(n + "," + src + "," + dest);
+            return;
+        }
+
+        // Move n-1 disks from source to helper
+        TOW(n - 1, src, dest, helper);
+
+        // Move nth disk from source to destination
+        System.out.println(n + "," + src + "," + dest);
+
+        // Move n-1 disks from helper to destination
+        TOW(n - 1, helper, src, dest);
+    }
+
+    public static void main(String[] args) {
+
+        int n = 3;
+
+        TOW(n, 1, 2, 3);
+    }
 }
