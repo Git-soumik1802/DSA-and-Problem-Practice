f6cf7022807118156a36701c7c7bc0ce9430115e
Git-soumik1802 <128280509+Git-soumik1802@users.noreply.github.com>
2026-09-05 21:54:05 +0530

    Add new DSA problems

---
+++ src/DynamicProgramming/fibonacci.java
@@ -0,0 +1,21 @@
+package DynamicProgramming;
+import java.util.Arrays;
+public class fibonacci {
+static int dp[];
+	 public static int fib(int n) {
+		 if(n<=1) {
+			 return n;
+		 }
+		 if(dp[n]!=-1) {
+			 return dp[n];
+		 }
+		 dp[n]=fib(n-1)+fib(n-2);
+		 return dp[n];
+	 }
+	public static void main(String[] args) {
+		 int n = 5;
+	        dp = new int[n + 1];
+	        Arrays.fill(dp, -1);
+	        System.out.println(fib(n));
+	}
+}
