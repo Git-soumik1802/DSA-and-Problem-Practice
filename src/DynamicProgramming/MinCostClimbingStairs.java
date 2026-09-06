8921c8da82887e170578129698b0809e9ddc92d8
Git-soumik1802 <128280509+Git-soumik1802@users.noreply.github.com>
2026-09-06 19:49:20 +0530

    Add new DSA problems

---
+++ src/DynamicProgramming/MinCostClimbingStairs.java
@@ -0,0 +1,39 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+public class MinCostClimbingStairs {
+	  public static int minCostClimbingStairs(int[] cost) {
+	        int n=cost.length;
+	     int  dp[]=new int[n+1];
+	        dp[0]=0;
+	        dp[1]=0;
+	        for(int i=2;i<=n;i++){
+	            dp[i]=Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+	        }
+	        return dp[n];
+	    }
+	  public static int minCostClimbingStairswithoutDP(int cost[]) {
+		  int n=cost.length;
+		  int prev1=0;
+		  int prev2=0;
+		  for(int i=2;i<=n;i++) {
+			  int curr=Math.min(prev1+cost[i-1], prev2+cost[i-2]);
+			  prev2=prev1;
+			  prev1=curr;
+		  }
+		  return prev1;
+	  }
+	  public static void main(String[] args) {
+		  int[] cost = {1,100,1,1,1,100,1,1,100,1};
+
+	        int result = minCostClimbingStairs(cost);
+	        int res=minCostClimbingStairswithoutDP(cost);
+
+	        System.out.println("Cost: " + Arrays.toString(cost));
+	        System.out.println("Minimum Cost: " + result);
+	        System.out.println("Cost: " + Arrays.toString(cost));
+	        System.out.println("Minimum Cost: " + res);
+	}
+
+}
--- src/DynamicProgramming/fibonacci.java
+++ src/DynamicProgramming/fibonacci.java
@@ -12,10 +12,19 @@ static int dp[];
 		 dp[n]=fib(n-1)+fib(n-2);
 		 return dp[n];
 	 }
+	 public static int fiboTab(int n) {
+		 dp[0]=0;
+		 dp[1]=1;
+		 for(int i=2;i<=n;i++) {
+			 dp[i]=dp[i-1]+dp[i-2];
+		 }
+		 return dp[n];
+	 }
 	public static void main(String[] args) {
 		 int n = 5;
 	        dp = new int[n + 1];
 	        Arrays.fill(dp, -1);
 	        System.out.println(fib(n));
+	        System.out.println(fiboTab(n));
 	}
 }
